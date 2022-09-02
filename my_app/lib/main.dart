652887ba3df5acd3de23ef6e714780bece268864
mackingth <mituemccone@gmail.com>
2022-09-02 12:20:05 +0900

    set調べる

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -313,6 +313,54 @@ class _TargetPostPageState extends State<TargetPostPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // 投稿メッセージ入力
+              Expanded(
+                // Stream
+                // 非同期処理の結果を元にWidgetを作れる
+                child: StreamBuilder<QuerySnapshot>(
+                  // 投稿メッセージ一覧を取得（非同期処理）
+                  // 投稿日時でソート
+                  stream: FirebaseFirestore.instance
+                      .collection('posts')
+                      .orderBy('date')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    // データが取得できた場合
+                    if (snapshot.hasData) {
+                      final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                      // 取得した投稿メッセージ一覧を元にリスト表示
+                      return ListView(
+                        children: documents.map((document) {
+                          return Card(
+                            child: ListTile(
+                              title: Text(document['text']),
+                              subtitle: Text(document['email']),
+                              // 自分の投稿メッセージの場合は削除ボタンを表示
+                              /*
+                              trailing: document['email'] == user.email
+                                  ? IconButton(
+                                      icon: Icon(Icons.delete),
+                                      onPressed: () async {
+                                        // 投稿メッセージのドキュメントを削除
+                                        await FirebaseFirestore.instance
+                                            .collection('posts')
+                                            .doc(document.id)
+                                            .delete();
+                                      },
+                                    )
+                                  : null,
+                                */
+                            ),
+                          );
+                        }).toList(),
+                      );
+                    }
+                    // データが読込中の場合
+                    return Center(
+                      child: Text('読込中...'),
+                    );
+                  },
+                ),
+              ),
               TextFormField(
                 decoration: InputDecoration(labelText: '目標'),
                 // 複数行のテキスト入力
@@ -329,7 +377,7 @@ class _TargetPostPageState extends State<TargetPostPage> {
               Container(
                 width: double.infinity,
                 child: ElevatedButton(
-                  child: Text('投稿'),
+                  child: Text('設定'),
                   onPressed: () async {
                     final date =
                         DateTime.now().toLocal().toIso8601String(); // 現在の日時
@@ -401,7 +449,7 @@ class _AchievementPostPageState extends State<AchievementPostPage> {
               Container(
                 width: double.infinity,
                 child: ElevatedButton(
-                  child: Text('投稿'),
+                  child: Text('更新'),
                   onPressed: () async {
                     final date =
                         DateTime.now().toLocal().toIso8601String(); // 現在の日時
